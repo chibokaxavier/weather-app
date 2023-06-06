6193d40e9c30768702b60234a079d8ebe0f79fd4
manlikexavy <chibokaxavier@gmail.com>
2023-06-06 03:26:09 -0700

    FIRST COMMIT

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import axios  from 'axios'
+import {BsSearch} from "react-icons/bs"
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid={process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  
   return (
-   <main>
-    
-   </main>
-  )
+    <>
+      <Head>
+        <title>WEATHER APP</title>
+      </Head>
+      <main className="bg-red-300">
+        <h1>Hello World</h1>
+      </main>
+    </>
+  );
 }
